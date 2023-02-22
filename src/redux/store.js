547900f1b4e42dc41d4bfc4e55f603f9e737284b
imtiajul-islam-d <imtiajul@hotmail.com>
2023-02-22 21:35:43 +0600

    myLogger middleware custom made added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,13 @@
-import { createStore } from "redux";
+import { applyMiddleware, createStore } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+// myLogger Middleware
+const myLogger = (store) => (next) => (action) => {
+  console.log("Action:", JSON.stringify(action));
+  console.log("Previous State:", store.getState());
+  const upComingState = [action].reduce(rootReducer, store.getState());
+  console.log("Upcoming State:", JSON.stringify(upComingState));
+  next(action)
+};
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 export default store;
